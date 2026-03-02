790845ebb7f9cd033de713c97ed7ca1fa410c4ee
Aram Margaryan <marg-aa@mail.ru>
2026-03-02 18:10:41 +0300

    Third attempt

--- IJ ДЗ Счетчик/Assets/Scripts/Counter.cs
+++ IJ ДЗ Счетчик/Assets/Scripts/Counter.cs
@@ -1,57 +1,28 @@
 using System;
-using System.Collections;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-    public event Action<int> TextRunning;
-
-    private Coroutine _coroutine;
-
     [SerializeField] private UserInputter _userInputter;
 
-    private float _delay = 0.5f;
     private bool _canRun = false;
-    private int _count;
+
+    public event Action<bool> MouseButtonClicked;
 
     private void OnEnable()
     {
-        _userInputter.MouseButtonClicked += Run;
+        _userInputter.MouseButtonClicked += RunViewer;
     }
 
     private void OnDisable()
     {
-        _userInputter.MouseButtonClicked -= Run;
+        _userInputter.MouseButtonClicked -= RunViewer;
     }
 
-    private void Run()
+    private void RunViewer()
     {
         _canRun = !_canRun;
 
-        if (_canRun)
-        {
-            _coroutine = StartCoroutine(CountUp(_delay));
-        }
-        else
-        {
-            if (_coroutine != null)
-            {
-                StopCoroutine(_coroutine);
-            }
-        }
+        MouseButtonClicked?.Invoke(_canRun);
     }
-
-    private IEnumerator CountUp(float delay)
-    {
-        WaitForSeconds wait = new(delay);
-
-        while (enabled)
-        {
-            TextRunning?.Invoke(_count);
-
-            _count++;
-
-            yield return wait;
-        }
-    }    
 }
--- IJ ДЗ Счетчик/Assets/Scripts/CounterViewer.cs
+++ IJ ДЗ Счетчик/Assets/Scripts/CounterViewer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -7,14 +8,20 @@ public class CounterViewer : MonoBehaviour
 
     [SerializeField] private Counter _counter;
 
+    private Coroutine _coroutine;
+
+    private float _delay = 0.5f;
+
+    private int _count;
+
     private void OnEnable()
     {
-        _counter.TextRunning += Display;
+        _counter.MouseButtonClicked += RunCounter;
     }
 
     private void OnDisable()
     {
-        _counter.TextRunning -= Display;
+        _counter.MouseButtonClicked -= RunCounter;
     }
 
     private void Start()
@@ -22,6 +29,35 @@ public class CounterViewer : MonoBehaviour
         _text.text = "0";
     }
 
+    private void RunCounter(bool mustRun)
+    {
+        if (mustRun)
+        {
+            _coroutine = StartCoroutine(CountUp(_delay));
+        }
+        else
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+            }
+        }
+    }
+
+    private IEnumerator CountUp(float delay)
+    {
+        WaitForSeconds wait = new(delay);
+
+        while (enabled)
+        {
+            Display(_count);
+
+            _count++;
+
+            yield return wait;
+        }
+    }
+    
     private void Display(int count)
     {
         _text.text = count.ToString();
